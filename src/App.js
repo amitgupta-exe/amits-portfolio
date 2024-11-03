8a326a6a5fa1f92bcf11bf77d1c9ba8798a70d87
Amit Gupta <72860177+amitgupta-exe@users.noreply.github.com>
2024-11-04 03:16:16 +0530

    removed video, added image, changed sidebar

--- src/App.js
+++ src/App.js
@@ -14,11 +14,11 @@ const Sidebar = () => {
   return (
     <nav className="sidebar">
       <ul>
-        <li><a href="#me">Me</a></li>
-        <li><a href="#ml-ai">ML/AI</a></li>
-        <li><a href="#webdev">Web Development</a></li>
-        <li><a href="#gamedev">Game Development</a></li>
-        <li><a href="#data-analysis">Data Analysis</a></li>
+        <li> <p>Me</p><a href="#me"></a></li>
+        <li><p>ML/AI</p><a href="#ml-ai"></a></li>
+        <li><p>Web Development</p><a href="#webdev"></a></li>
+        <li><p>Game Development</p><a href="#gamedev"></a></li>
+        <li><p>Data Analysis</p><a href="#data-analysis"></a></li>
       </ul>
     </nav>
   );
@@ -41,7 +41,13 @@ const Me = () => (
     <div className="text-container">
       <h1>Amit Gupta</h1>
       <h2>Software Engineer / Data Scientist</h2>
-      <p>I create stuff</p>
+      <p>Bsc - Computer Science</p>
+      <p>Msc - Data Science and Big Data Analytics</p>
+      <div className="socials">
+        <a target='_blank' href="https://www.linkedin.com/in/amit-gupta-609306264/">LinkedIn | </a>
+        <a target='_blank' href="https://github.com/amitgupta-exe">GitHub</a>
+      </div>
+
     </div>
     <div className="image-container">
       <img src="img.png" alt="Amit Gupta" />
@@ -61,8 +67,8 @@ const webDevUrls = [
 ];
 
 const gameDevUrls = [
-  { videoUrl: 'your-gamedev-video-url-1.mp4', description: 'Game Dev Project 1', github: 'https://github.com/project1', website: 'https://yourwebsite.com/project1' },
-  { videoUrl: 'your-gamedev-video-url-2.mp4', description: 'Game Dev Project 2', github: 'https://github.com/project2', website: 'https://yourwebsite.com/project2' },
+  { videoUrl: "/image.png", description: 'Snake Game p5', github: 'https://github.com/amitgupta-exe/snakegamep5', website: 'https://amitgupta-exe.github.io/snakegamep5' },
+  { videoUrl: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4', description: 'Game Dev Project 2', github: 'https://github.com/project2', website: 'https://yourwebsite.com/project2' },
   { videoUrl: 'your-gamedev-video-url-3.mp4', description: 'Game Dev Project 3', github: 'https://github.com/project3', website: 'https://yourwebsite.com/project3' },
 ];
 
@@ -72,26 +78,24 @@ const dataAnalysisUrls = [
   { videoUrl: 'your-data-analysis-video-url-3.mp4', description: 'Data Analysis Project 3', github: 'https://github.com/project3', website: 'https://yourwebsite.com/project3' },
 ];
 
-const MLAI = () => <Section title="ML/AI" id="ml-ai" urls={mlAiUrls} />;
-const WebDev = () => <Section title="Web Development" id="webdev" urls={webDevUrls} />;
-const GameDev = () => <Section title="Game Development" id="gamedev" urls={gameDevUrls} />;
-const DataAnalysis = () => <Section title="Data Analysis" id="data-analysis" urls={dataAnalysisUrls} />;
+const MLAI = () => <Section title="ML/AI" id="ml-ai" cards={mlAiUrls} />;
+const WebDev = () => <Section title="Web Development" id="webdev" cards={webDevUrls} />;
+const GameDev = () => <Section title="Game Development" id="gamedev" cards={gameDevUrls} />;
+const DataAnalysis = () => <Section title="Data Analysis" id="data-analysis" cards={dataAnalysisUrls} />;
 
-const Section = ({ title, id, urls }) => {
+const Section = ({ title, id, cards }) => {
   return (
     <div className="section" id={id}>
       <h2>{title}</h2>
       <div className="vertical-sections">
-        {urls.map((num) => (
-          <div className="vertical-section" key={num.videoUrl}>
-            <video className="video" autoPlay loop muted>
-              <source src={num.videoUrl} type="video/mp4" /> {/* Replace with your video URL */}
-              Your browser does not support the video tag.
-            </video>
+        {cards.map((card) => (
+          <div className="vertical-section" key={card.videoUrl}>
+            <img src={card.videoUrl} alt="" srcset="" />
+
             <div className="card">
-              <p>Description for section {num.description}</p>
-              <a href="https://github.com" target="_blank" rel="noopener noreferrer">GitHub</a>
-              <a href="https://yourwebsite.com" target="_blank" rel="noopener noreferrer">Website</a>
+              <p>{card.description}</p>
+              <a href={card.github} target="_blank" rel="noopener noreferrer">GitHub</a>
+              <a href={card.website} target="_blank" rel="noopener noreferrer">Website</a>
             </div>
           </div>
         ))}
